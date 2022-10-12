8a7dec8db78360dbfd2bcb2e3f52cc0dabfa51bf
hashiyah <115485747+hashiyah@users.noreply.github.com>
2022-10-12 15:29:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,77 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94771163079'] //ur owner number
+global.ownername = "HASHIYAH" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
+global.owner = ['94771163079']
 global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.botnumber = '94771163079' //Add your Bot Number For Log
+global.botname = 'CYBER-X HASHIYAH BOT' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
+global.packname = "Cyber-X HASHIYAH Bot" //ur sticker watermark packname
 global.author = "DARK ALPHA" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+█-----╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗-----█
+█-----║║║╠─║─║─║║║║║╠─-----█
+█-----╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝-----█
+█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+    
+             හ SHI ය"හ්😈
+      BOT IS ALIVE NOW!!!
+
+
+❌WIVE COMMAND TYPE👇👇👇
+☠.menu
+
+❌BOT ONLINE TEST👇👇👇
+☠.alive
+
+❌SONG DOWNLOAD👇👇👇
+☠.song
+(.song lelena)
+
+❌VIDEO DOWNLOAD👇👇👇
+☠.video (youtube link)
+
+❌DEVELOPER
+☠(හ SHI ය'හ්😈)
+
+❌(හ SHI ය'හ්😈)👇👇👇
+☠http://wa.me/+94771163079?text=.alive
+
+😈FULL RESPECT THIS BOT
+
+(¯`v´¯)　　
+ ` .¸. ´
+☻/
+/▌
+/ |
+
+😈GOOD LUCK
+
+┏╗ ┏╗
+║┃ ║┃╔━╦╦┳═╗
+║┃ ┃╚┫║┃┃┃╩┫
+┗╝ ╚━╩═┻━╩━╝
+┓╔┓┏
+║╚┛┣═╦┳╗
+┗╗┏╣┃┃║┃
+   ┗╝┗═┻═╝"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
